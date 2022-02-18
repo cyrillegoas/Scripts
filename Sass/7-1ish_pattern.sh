d3477fd1f87eff579e083ad8fcd9e63731fafbc2
Cyril Le Goas <cyril.legoas@gmail.com>
2022-02-18 14:22:54 +0100

    ADD Sass archi creator

---
+++ Sass/7-1ish_pattern.sh
@@ -0,0 +1,11 @@
+# create folder archi
+mkdir scss scss/abstracts scss/base scss/components scss/layout scss/pages
+cd scss
+# create main scss file
+touch styles.scss
+# create abstracts files
+touch abstracts/_index.scss abstracts/_mixins.scss abstracts/_functions.scss abstracts/_placeholders.scss abstracts/_colors.scss abstracts/_fonts.scss abstracts/_breakpoints.scss
+# create base files
+touch base/_index.scss base/_base.scss base/_reset.scss base/_typography.scss  base/_font-face.scss
+#create index files for components / layout / pages
+touch components/_index.scss layout/_index.scss pages/_index.scss
